d502c4a2ba669e6b3aa8977a457b7f44da520de7
KimEd <ked81061@gmail.com>
2020-12-17 19:26:38 +0900

    Added MyInfoWindow

---
+++ 3D_RPG_Project/Assets/_3D RPG/Scripts/UI/MyInfo/MyInfoMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyInfoMenu : MonoBehaviour
+{
+    [SerializeField] GameObject goMyInfoMenu = null;
+    [SerializeField] Text txtMyName = null;
+    [SerializeField] Text txtLevel = null;
+    [SerializeField] Text txtStr = null;
+    [SerializeField] Text txtDex = null;
+    [SerializeField] Text txtDef = null;
+    [SerializeField] Text txtHp = null;
+    [SerializeField] Text txtMp = null;
+    [SerializeField] Image imgUser = null;
+
+    bool isOpen = false;
+
+    // PlayerStatus thePlayerStatus;
+
+    void Awake()
+    {
+        //thePlayerStatus = FindObjectOfType<PlayerStatus>();
+    }
+
+    public void OnTouchMenu()
+    {
+        isOpen = !isOpen;
+
+        if (isOpen) ShowMenu();
+        else HideMenu();
+    }
+    
+    void ShowMenu() {
+        goMyInfoMenu.SetActive(true);
+    }
+
+    public void HideMenu()
+    {
+        goMyInfoMenu.SetActive(false);
+    }
+}

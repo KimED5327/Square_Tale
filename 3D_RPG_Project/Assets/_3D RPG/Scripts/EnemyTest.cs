f8243cab165f01668a78a01435edead9235de7f5
DESKTOP-2DTOUAQ\ehdrj <ehdrjs4773@naver.com>
2020-12-18 09:20:03 +0900

    Enemy Ai 기초 작업

---
+++ 3D_RPG_Project/Assets/_3D RPG/Scripts/EnemyTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest : MonoBehaviour
+{
+
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+    }
+}

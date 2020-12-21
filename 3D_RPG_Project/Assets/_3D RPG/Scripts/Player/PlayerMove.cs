cab335df3528cc1045241f0799a871d0737c10a9
kwanhee <puga3648@naver.com>
2020-12-21 20:22:45 +0900

    player, camera 기본 기능, 무료asset 적용

--- 3D_RPG_Project/Assets/_3D RPG/Scripts/Player/PlayerMove.cs
+++ 3D_RPG_Project/Assets/_3D RPG/Scripts/Player/PlayerMove.cs
@@ -9,24 +9,85 @@ public class PlayerMove : MonoBehaviour
     float hAxis;
     float vAxis;
 
+    bool isJump;
+    bool isDodge;
+
     Vector3 moveVec;
+    Vector3 dodgeVec;
+
+    Rigidbody myRigid;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigid = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+        Jump();
+        Dodge();
+    }
+
+    void Move()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
 
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
+        if (isDodge)
+        {
+            moveVec = dodgeVec;
+        }
+
         transform.position += moveVec * speed * Time.deltaTime;
 
         transform.LookAt(transform.position + moveVec);
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown("c") && !isJump && !isDodge)
+        {
+            myRigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void Dodge()
+    {
+        if (Input.GetKeyDown("space") && !isJump && moveVec != Vector3.zero && !isDodge)
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.5f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 }

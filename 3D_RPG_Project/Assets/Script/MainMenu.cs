fcbc074b3c55331a6483d11f7264b8047ee6c5aa
Nia Kim <niakim63@gmail.com>
2020-12-22 11:21:56 +0900

    다시푸시

---
+++ 3D_RPG_Project/Assets/Script/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickMainGame()
+    {
+        LoadingScene.LoadScene("Scene2");
+    }
+
+    public void OnClickLoad()
+    {
+        LoadingScene.LoadScene("Scene2");
+    }
+
+    public void OnClickOption()
+    {
+        LoadingScene.LoadScene("Scene2");
+    }
+
+    public void OnClickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}

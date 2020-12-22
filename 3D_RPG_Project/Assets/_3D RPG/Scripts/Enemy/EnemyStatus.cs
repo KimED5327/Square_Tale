596f9bb27979f96b11e9f81cab42772b3d35aee7
KimEd <ked81061@gmail.com>
2020-12-22 20:31:23 +0900

    Added Drop - Rooting System - Push Inventory

---
+++ 3D_RPG_Project/Assets/_3D RPG/Scripts/Enemy/EnemyStatus.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStatus : Status
+{
+    [SerializeField]
+    DropItemInfo[] _dropItemInfo;
+
+    List<DropItem> _dropItemList = new List<DropItem>();
+
+    bool _isDropItem = false;
+
+    public void Update()
+    {
+        // 임시. 죽으면 DesideDropItem을 호출한 이후에 지울 것.
+        if (!_isDead)
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                Debug.Log("몬스터 사망했으니, 시체를 터치하여 아이템 획득을 시도하세요.");
+                _isDead = true;
+                DesideDropItem();
+            }
+        }
+
+    }
+
+
+
+    // 죽으면 실행
+    public void DesideDropItem()
+    {
+        // 각 아이템별 랜덤 드롭 확률에 걸리면 드롭 리스트에 추가
+        for(int i = 0; i < _dropItemInfo.Length; i++)
+        {
+            float random = Random.Range(0f, 100f);
+            if(random <= _dropItemInfo[i].itemDropRate)
+            {
+                DropItem dropItem = new DropItem{
+                    itemID = _dropItemInfo[i].itemID,
+                    itemCount = Random.Range(_dropItemInfo[i].itemCounts.min, _dropItemInfo[i].itemCounts.max + 1)
+                };
+                _dropItemList.Add(dropItem);
+            }
+        }
+
+        // 드롭 리스트에 하나라도 있으면 드롭 OK
+        if(_dropItemList.Count > 0)
+        {
+            _isDropItem = true;
+        }
+    }
+
+    public List<DropItem> GetDropItem()
+    {
+        if (!_isDropItem)
+            return null;
+
+        return _dropItemList;
+    }
+
+
+    public void SetDropItem(List<DropItem> leftDropitem) 
+    {
+        if (leftDropitem.Count > 0)
+            _dropItemList = leftDropitem;
+        else
+        {
+            _dropItemList.Clear();
+            _isDropItem = false;
+        }
+    }
+
+    //public void Initialized()
+    //{
+    //    _isDropItem = false;
+    //    _dropItemList.Clear();
+    //}
+}
---
+++ 3D_RPG_Project/Assets/_3D RPG/Scripts/UI/Rooting/RootingSlot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class RootingSlot : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] Text _txtName = null;
+    [SerializeField] Text _txtType = null;
+    [SerializeField] Text _txtCount = null;
+    [SerializeField] Image _ImgIcon = null;
+
+    Item _item;
+    int _count;
+
+    Rooting _rootingUI;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _rootingUI.PushInventory(_item, _count);
+    }
+
+    public void SetLink(Rooting rooting) => _rootingUI = rooting;
+
+    public void SetSlot(Item item, int count, bool isGold)
+    {
+        _item = item;
+        _count = count;
+
+        _txtName.text = _item.name;
+        _txtType.text = (_item.type == ItemType.WEAPON) ? "무기"
+                      : (_item.type == ItemType.ARMOR)  ? "방어구"
+                      : (_item.type == ItemType.ETC)    ? "재화"
+                                                        : "기타";
+
+        if(!isGold)
+            _txtCount.text = $"X {0}" + _count;
+        else
+            _txtCount.text = $"{0} Gold" + _count;
+
+        _ImgIcon.sprite = _item.sprite;
+    }
+}

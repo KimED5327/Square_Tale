05b62ad8ec9bc4769f70410c29a1835e85dbd9d7
DESKTOP-2DTOUAQ\ehdrj <ehdrjs4773@naver.com>
2020-12-28 16:17:32 +0900

    사운드 매니저

--- 3D_RPG_Project/Assets/_3D RPG/Scripts/Database/SoundManager.cs
+++ 3D_RPG_Project/Assets/_3D RPG/Scripts/Database/SoundManager.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 public class SoundInfo
 {
     public string name;
-    public AudioClip audio;
-    public float pithc;
+    public AudioClip clip;
 }
 
 public class SoundManager : MonoBehaviour
@@ -16,38 +15,87 @@ public class SoundManager : MonoBehaviour
     public static SoundManager instance;
 
     //풀 정보
-    [SerializeField] SoundInfo[] _sound = null;
+    [SerializeField] SoundInfo[] effectSounds;
+    [SerializeField] AudioSource[] effectPlayer;
 
-    //풀 자료구조
-    Queue<AudioClip>[] _queues = null;
-
-    //풀 딕셔너리
-    Queue<AudioClip> _keepQueue;
+    [SerializeField] SoundInfo[] bgmSound;
+    [SerializeField] AudioSource bgmPlayer;
 
     private void Awake()
     {
         if(instance == null)
         {
             instance = this;
-
-            DontDestroyOnLoad(this.gameObject);
-            _queues = new Queue<AudioClip>[_sound.Length];
-
-            for(int i = 0; i < _sound.Length; ++i)
-            {
-                //_queues[i] = MakePool
-            }
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 
-    Queue<AudioClip> MakePool(AudioClip audio, float pithc)
+    void PlayBGM(string p_name)
     {
-        Queue<AudioClip> queue = new Queue<AudioClip>();
+        for(int i = 0;i < bgmSound.Length; i++)
+        {
+            if(p_name == bgmSound[i].name)
+            {
+                bgmPlayer.clip = bgmSound[i].clip;
+                bgmPlayer.Play();
+                return;
+            }
+        }
 
-        
+        Debug.LogError(p_name + "에 해당되는 BGM이 없습니다");
 
-        return queue;
     }
 
+    void StopBGM()
+    {
+        bgmPlayer.Stop();
+    }
+    void PauseBGM()
+    {
+        bgmPlayer.Pause();
+    }
+    void UnPauseBGM()
+    {
+        bgmPlayer.UnPause();
+    }
+    void PlayEffectSound(string p_name)
+    {
+        for(int i = 0; i < effectSounds.Length;i++)
+        {
+            if(p_name == effectSounds[i].name)
+            {
+                for(int j = 0; j < effectPlayer.Length; j++)
+                {
+                    if(!effectPlayer[j].isPlaying)
+                    {
+                        effectPlayer[j].clip = effectSounds[i].clip;
+                        effectPlayer[j].Play();
+                        return;
+                    }
+                    
+                }
+                Debug.LogError("모든 효과음 플레이어가 사용중입니다.");
+                return;
+            }
+            Debug.LogError(p_name + "에 해당하는 효과음 사운드가 없습니다");
+        }
+    }
 
+    void StopAllEffectSound()
+    {
+        for(int i = 0; i < effectPlayer.Length; i++)
+        {
+            effectPlayer[i].Stop();
+        }
+    }
+
+    public void PlaySound(string p_name, int p_Type)
+    {
+        if (p_Type == 0) PlayBGM(p_name);
+        else if (p_Type == 1) PlayEffectSound(p_name);
+    }
 }

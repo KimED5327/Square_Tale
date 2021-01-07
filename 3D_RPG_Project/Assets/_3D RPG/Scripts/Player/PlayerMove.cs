6cb317849831a7902f2ad12b166f5a8db939ca87
kwanhee <puga3648@naver.com>
2021-01-07 11:12:10 +0900

    player->playermove

--- 3D_RPG_Project/Assets/_3D RPG/Scripts/Player/PlayerMove.cs
+++ 3D_RPG_Project/Assets/_3D RPG/Scripts/Player/PlayerMove.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{ 
     public float speed;
     public float jumpForce;
     public GameObject attackEffect1;

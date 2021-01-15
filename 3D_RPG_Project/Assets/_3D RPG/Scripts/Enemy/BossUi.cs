78cbb847b6a52dae926c553e2ad8b97d9c960bd4
DESKTOP-2DTOUAQ\ehdrj <ehdrjs4773@naver.com>
2021-01-15 14:31:01 +0900

    몬스터 기본 공격 범위 증가 // 보스 UI작업중

---
+++ 3D_RPG_Project/Assets/_3D RPG/Scripts/Enemy/BossUi.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BossUi : MonoBehaviour
+{
+
+    public GameObject _boss;
+    EnemyStatus _BossStatus;
+    public Slider hpBar;
+    public Text _Ui;
+    float hpPercent;
+
+    void Start()
+    {
+
+        _BossStatus = _boss.GetComponent<EnemyStatus>();
+        hpBar = GetComponentInChildren<Slider>();
+    }
+
+    void Update()
+    {
+
+        hpPercent = (_BossStatus.GetCurrentHp() / (float)_BossStatus.GetMaxHp()) * 100;
+
+        if (hpBar == null)
+        {
+            hpBar = GetComponentInChildren<Slider>();
+        }
+
+
+        hpBar.value = _BossStatus.GetCurrentHp() / (float)_BossStatus.GetMaxHp();
+    }
+}

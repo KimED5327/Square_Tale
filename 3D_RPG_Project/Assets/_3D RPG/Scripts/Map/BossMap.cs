90fec84631b54947779ce7b6461956de2d811483
DESKTOP-2DTOUAQ\ehdrj <ehdrjs4773@naver.com>
2021-01-25 16:35:24 +0900

    원거리 공격 맞을 시 추격 상태로 변경 / 몬스터 UI크기 수정 / 보스 맵 블럭 이름 변경(아직 작업은 안함)

--- 3D_RPG_Project/Assets/_3D RPG/Scripts/Map/BossMap.cs
+++ 3D_RPG_Project/Assets/_3D RPG/Scripts/Map/BossMap.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class BossMap : MonoBehaviour
 {
+    public ParticleSystem particle1;
+    public ParticleSystem particle2;
+    public Boss boss;
 
+    private void Update()
+    {
+        if(boss == null)
+            GameObject.FindGameObjectWithTag("Enemy").GetComponent<Boss>();
+
+        if (boss.getIsDie())
+        {
+            particle1.gameObject.SetActive(true);
+            particle2.gameObject.SetActive(true);
+        }
+       else
+        {
+            particle1.gameObject.SetActive(false);
+            particle2.gameObject.SetActive(false);
+        }
+    }
 }

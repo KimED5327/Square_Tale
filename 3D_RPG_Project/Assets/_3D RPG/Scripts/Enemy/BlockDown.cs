8166410da0aa6676ca6069ac49f0e90164eb210b
DESKTOP-2DTOUAQ\ehdrj <ehdrjs4773@naver.com>
2021-01-27 10:01:05 +0900

    보스 프리팹 // BlockDown 스크립트 추가 // 씬 추가 씬은 왜있는지 모르겠네;

---
+++ 3D_RPG_Project/Assets/_3D RPG/Scripts/Enemy/BlockDown.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDown : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] _BlockGroup;
+    static bool[] _GroupDown = new bool[10];
+    private float _timer;
+
+    void Start()
+    {
+        _timer = 0f;
+
+        for(int i = 0; i < 10; i++)
+        {
+            _GroupDown[i] = false;
+        }
+    }
+
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if(_timer > 30)
+        {
+            for(int i = 0; i < _BlockGroup.Length; i++)
+            {
+                if (_GroupDown[i] == false)
+                {
+                  
+                    _GroupDown[i] = true;
+                    _timer = 0;
+                    break;
+                }
+
+            }
+        }
+
+
+        Debug.Log(_timer);
+
+        for(int i = 0; i < 10; i++)
+        {
+            if(_GroupDown[i] == false) return;
+            if(_GroupDown[i] == true)
+            {
+                _BlockGroup[i].transform.position = new Vector3(_BlockGroup[i].transform.position.x, _BlockGroup[i].transform.position.y - 0.1f, _BlockGroup[i].transform.position.z);
+
+                if(_BlockGroup[i].transform.position.y < -10)
+                {
+                   _BlockGroup[i].gameObject.SetActive(false);
+                }
+            }
+
+        }
+    }
+}
